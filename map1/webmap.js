5d979a4c7118194bd6e93021749a8b3d94d111ab
unknown <admin@Himes125B.lsu.edu>
2019-03-22 13:00:03 -0500

    UPDATE

--- map1/webmap.js
+++ map1/webmap.js
@@ -4,14 +4,21 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z
 	//maxZoom: 18,
 //	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 //}).addTo(myMap);
-let nolaPoint = L.marker([30, -90]).addTo(myMap)
+let btPoint = L.marker([30.4507, -91.1546]).addTo(myMap)
+let orPoint = L.marker([28.5421, -81.379]).addTo(myMap)
 let myTriangle = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-  //,change because this location is in London
-  //[]
+  [58.3019, -134.4197],
+  [44.3105, -69.7793],
+  [18,-66.590],
+  [34,-118.2439]
+  ]).addTo(myMap)
+  //,change because the location to a point of interest
+  //coordinates [latitude, longitude]
+  let myLine = L.polygon([
+    [30.4507, -91.1546],
+    [28.5421, -81.379] // L.line is NOT a function; it will pops out an error
 ]).addTo(myMap);
-nolaPoint.bindPopup('A <em><strong>marker</strong></em>') // take argument of the content inside the popup, this accept html
-myTriangle.bindPopup('A <em>POLIGON</em>')
-// figure it out how to create a line
+btPoint.bindPopup('<em><strong>Baton Rouge, LA</strong></em>') // take argument of the content inside the popup, this accept html
+orPoint.bindPopup('<em><strong>Orlando, Fl</strong></em>')
+myLine.bindPopup('<em>9h-43m distance between Baton Rouge,LA and Orlando,FL</em>') // figure it out how to create a line
+myTriangle.bindPopup('<em><strong>USA TERRITORY</strong></em>')
