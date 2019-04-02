9a56701363065d23f8ecf9a68ada372220a7e287
Luz M Castro <lcastr7@lsu.edu>
2019-04-02 10:57:50 -0500

    Leaflet Ass_3

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,7 +1,7 @@
 let myMap3 = L.map('map3').setView([38.75, -102.83], 4.48) // modify lat and long, zoom is good! might change tilte to some descriptive
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+ maxZoom: 17,
+ attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(myMap3);
 L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/ridge.cgi?sector=MSY&prod=N0Q', { //at New Orleans
  layers: 'single',
