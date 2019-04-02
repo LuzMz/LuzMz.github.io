e2b0fe4bc3339c6be316813153c6c4815243c89d
unknown <admin@Himes125B.lsu.edu>
2019-04-02 10:12:47 -0500

    Leaflet Ass_2

--- map5/webmap5.js
+++ map5/webmap5.js
@@ -3,18 +3,18 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(myMap5);
-L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/ridge.cgi?sector=DGX&prod=N0Q', {
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/ridge.cgi?sector=MSY&prod=N0Q', { //at New Orleans
  layers: 'single',
  format: 'image/png',
  transparent: true,
  attribution: 'NOAA, Iowa State University'
  }).addTo(myMap5);
- //L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi', {
-  //layers: 'mrms_p72h',
-  //format: 'image/png',
-  //transparent: true,
-  //attribution: 'NOAA, Iowa State University'
-  //}).addTo(myMap5)
+ L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi', { // in USA
+  layers: 'mrms_p72h',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+  }).addTo(myMap5)
 //L.esri.dynamicMapLayer({
   //url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
 	//}).addTo(myMap5) // dinamic map layer: take the url on our SGI dollowing its standars  /* .notes..  */
