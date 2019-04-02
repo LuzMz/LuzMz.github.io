45ff403e31df8bd77f628c8ab626742a222f6ccf
Luz M Castro <lcastr7@lsu.edu>
2019-04-02 11:32:25 -0500

    Leaflet Ass_3

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -2,19 +2,8 @@ let myMap3 = L.map('map3').setView([38.75, -102.83], 4.48) // modify lat and lon
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
  maxZoom: 17,
  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(myMap3);
-L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/ridge.cgi?sector=MSY&prod=N0Q', { //at New Orleans
- layers: 'single',
- format: 'image/png',
- transparent: true,
- attribution: 'NOAA, Iowa State University'
-}).addTo(myMap3);
- L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi', { // in USA
-  layers: 'mrms_p72h',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
 }).addTo(myMap3)
-//L.esri.dynamicMapLayer({
-  //url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
-	//}).addTo(myMap3) // dinamic map layer: take the url on our SGI dollowing its standars  /* .notes..  */
+let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+//jQuery.getJSON(stateDemographicsUrl, function (data) { //from step6
+  //L.geoJSON(data).addTo(stateMap)
+//})
