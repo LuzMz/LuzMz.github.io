6b99fe5381ec7a5bdb04aee48ce2e23c9ea6afc7
unknown <admin@Himes125B.lsu.edu>
2019-05-02 16:09:26 -0500

    change map #

--- ffsconsulting/Project1/webmap1.js
+++ ffsconsulting/Project1/webmap1.js
@@ -1,4 +1,4 @@
-let stateMap = L.map('map4').setView([50.75, -110.83], 3.495) //modify scale and coordinates!! :)
+let stateMap = L.map('map1').setView([50.75, -110.83], 3.495) //modify scale and coordinates!! :)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
  maxZoom: 17,
  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
