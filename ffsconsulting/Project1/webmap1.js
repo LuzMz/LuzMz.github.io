ee5b4f7829a3ffc3c0798be3d6a9899f41bc451d
unknown <admin@Himes125B.lsu.edu>
2019-05-03 09:03:20 -0500

    coordinates zoom

--- ffsconsulting/Project1/webmap1.js
+++ ffsconsulting/Project1/webmap1.js
@@ -1,4 +1,4 @@
-let stateMap = L.map('map1').setView([31.1, -91.96], 7.5) //modify scale and coordinates!! :)
+let stateMap = L.map('map1').setView([30.015601, -90.097841], 16.5) //modify scale and coordinates!! :)
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
